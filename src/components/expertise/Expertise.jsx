71f91dc6509bbacd6fb59e70e6260ea79da8816f
Krishna Sahu <dvlprkrishna@gmail.com>
2023-08-22 12:11:43 +0530

    Added Cobe globe temp.

--- src/components/expertise/Expertise.jsx
+++ src/components/expertise/Expertise.jsx
@@ -2,43 +2,9 @@ import styles from "./expertise.module.css";
 
 import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
+import { useSpring } from "react-spring";
 const Expertise = () => {
-  const canvasRef = useRef();
-
-  useEffect(() => {
-    let phi = 0;
-
-    const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: 600 * 2,
-      height: 600 * 2,
-      phi: 3,
-      theta: 0,
-      dark: 1,
-      diffuse: 1.2,
-      mapSamples: 16000,
-      mapBrightness: 6,
-      baseColor: [0.3, 0.3, 0.3],
-      markerColor: [0.1, 0.8, 1],
-      glowColor: [1, 1, 1],
-      markers: [
-        // longitude latitude
-        { location: [37.7595, -122.4367], size: 0.03 },
-        { location: [40.7128, -74.006], size: 0.1 },
-      ],
-      onRender: (state) => {
-        // Called on every animation frame.
-        // `state` will be an empty object, return updated params.
-        state.phi = phi;
-        phi += 0.01;
-      },
-    });
-
-    return () => {
-      globe.destroy();
-    };
-  }, []);
-
+   
   return (
     <>
       <div
@@ -87,31 +53,116 @@ const Expertise = () => {
                 </div>
               </div>
             </div>
-            <div className="hidden relative md:flex col">
-              <canvas
-                ref={canvasRef}
-                style={{
-                  width: 600,
-                  height: 600,
-                  maxWidth: "100%",
-                  aspectRatio: 1,
-                }}
-              />
-              {/* <video
-                className="absolute bg-transparent top-0 left-0 w-full h-full object-cover z-0"
-                autoPlay
-                loop
-                muted
-              >
-                <source src="/expert-globe.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-              </video> */}
+            
+        <div className="hidden relative md:flex col">
+               <Cobe/> 
             </div>
           </div>
         </div>
+
       </div>
     </>
   );
 };
+
+export function Cobe() {
+  const canvasRef = useRef();
+  const pointerInteracting = useRef(null);
+  const pointerInteractionMovement = useRef(0);
+  const [{ r }, api] = useSpring(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
+  useEffect(() => {
+    let phi = 0;
+    let width = 0;
+    const onResize = () => canvasRef.current && (width = canvasRef.current.offsetWidth)
+    window.addEventListener('resize', onResize)
+    onResize()
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: width * 2,
+      height: width * 2,
+      phi: 0,
+      theta: 0.3,
+      dark: 1,
+      diffuse: 3,
+      mapSamples: 16000,
+      mapBrightness: 1.2,
+      baseColor: [1, 1, 1],
+      markerColor: [251 / 255, 100 / 255, 21 / 255],
+      glowColor: [1.2, 1.2, 1.2],
+      markers: [],
+      onRender: (state) => {
+        // This prevents rotation while dragging
+        if (!pointerInteracting.current) {
+          // Called on every animation frame.
+          // `state` will be an empty object, return updated params.
+          phi += 0.005
+        } 
+        state.phi = phi + r.get()
+        state.width = width * 2
+        state.height = width * 2
+      }
+    })
+    setTimeout(() => canvasRef.current.style.opacity = '1')
+    return () => globe.destroy()
+  }, [])
+  return <div style={{
+    width: '100%',
+    maxWidth: 600,
+    aspectRatio: 1,
+    margin: 'auto',
+    position: 'relative',
+  }}>
+    <canvas
+      ref={canvasRef}
+      onPointerDown={(e) => {
+        pointerInteracting.current =
+          e.clientX - pointerInteractionMovement.current;
+        canvasRef.current.style.cursor = 'grabbing';
+      }}
+      onPointerUp={() => {
+        pointerInteracting.current = null;
+        canvasRef.current.style.cursor = 'grab';
+      }}
+      onPointerOut={() => {
+        pointerInteracting.current = null;
+        canvasRef.current.style.cursor = 'grab';
+      }}
+      onMouseMove={(e) => {
+        if (pointerInteracting.current !== null) {
+          const delta = e.clientX - pointerInteracting.current;
+          pointerInteractionMovement.current = delta;
+          api.start({
+            r: delta / 200,
+          });
+        }
+      }}
+      onTouchMove={(e) => {
+        if (pointerInteracting.current !== null && e.touches[0]) {
+          const delta = e.touches[0].clientX - pointerInteracting.current;
+          pointerInteractionMovement.current = delta;
+          api.start({
+            r: delta / 100,
+          });
+        }
+      }}
+      style={{
+        width: '100%',
+        height: '100%',
+        cursor: 'grab',
+        contain: 'layout paint size',
+        opacity: 0,
+        transition: 'opacity 1s ease',
+      }}
+    />
+  </div>
+}
 
 export default Expertise;
