2c5a179f106adee63805f5d9b917ae5db0fcb9b4
dvlprkrishna <dvlprkrishna@gmail.com>
2023-08-28 02:02:16 +0530

    Refactored Code

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,38 @@
-'use client'
-import ACT from '@/components/ACT/ACT'
-import Announcement from '@/components/announcement/Announcement'
-import ClientLogos from '@/components/clientLogos/ClientLogos'
-import Copyright from '@/components/copyright/Copyright'
-import Expertise from '@/components/expertise/Expertise'
-import Footer from '@/components/footer/Footer'
-import InfotechWeekly from '@/components/infotechWeekly/InfotechWeekly'
-import Intro from '@/components/intro/Intro'
-import Navbar from '@/components/navbar/Navbar'
-import Projects from '@/components/projects/Projects'
-import Review from '@/components/review/Review'
-import Robot from '@/components/robot/Robot'
-import RobotDetails from '@/components/robotDetails/RobotDetails'
-import Subscribe from '@/components/subscribe/Subscribe'
-import Testimonial from '@/components/testimonial/Testimonial'
-import Image from 'next/image'
+"use client";
+import Announcement from "@/sections/announcement/Announcement";
+import ClientLogos from "@/sections/clientLogos/ClientLogos";
+import Copyright from "@/sections/copyright/Copyright";
+import Expertise from "@/sections/expertise/Expertise";
+import Footer from "@/sections/footer/Footer";
+import InfotechWeekly from "@/sections/infotechWeekly/InfotechWeekly";
+import Intro from "@/sections/intro/Intro";
+import Navbar from "@/sections/navbar/Navbar";
+import Projects from "@/sections/projects/Projects";
+import Review from "@/sections/review/Review";
+import Robot from "@/sections/robot/Robot";
+import RobotDetails from "@/sections/robotDetails/RobotDetails";
+import Subscribe from "@/sections/subscribe/Subscribe";
+import Testimonial from "@/sections/testimonial/Testimonial";
+import ACT from "@/sections/ACT/ACT";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <Announcement/> 
-      <Navbar/>
-      <Intro/>
-      <ClientLogos/>
-      <ACT/>
-      <Robot/>
-      <Expertise/>
-      <Projects/>
-      <Testimonial/>
-      <Review/>
-      <InfotechWeekly/>
-      <Subscribe/>
-      <Footer/>
-      <Copyright/>
+      <Announcement />
+      <Navbar />
+      <Intro />
+      <ClientLogos />
+      <ACT />
+      <Robot />
+      <Expertise />
+      <Projects />
+      <Testimonial />
+      <Review />
+      <InfotechWeekly />
+      <Subscribe />
+      <Footer />
+      <Copyright />
     </main>
-  )
+  );
 }
