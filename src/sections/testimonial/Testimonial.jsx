b4b8d5111d214333bc203d69fb71812883e83d69
dvlprkrishna <dvlprkrishna@gmail.com>
2023-08-30 14:12:59 +0530

    Testimonial Updated

--- src/sections/testimonial/Testimonial.jsx
+++ src/sections/testimonial/Testimonial.jsx
@@ -9,82 +9,84 @@ import 'swiper/css/pagination';
 import styles from './testimonial.module.css';
 
 // import required modules
-import { Autoplay,Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 import Image from "next/image";
 import Link from "next/link";
 
-import Icon from "@mdi/react";
-import { mdiArrowRight } from "@mdi/js";
+import Icon from "@mdi/react"; 
 import { motion } from "framer-motion";
+
+import { mdiDomain } from '@mdi/js';
+
 
 const Testimonials = () => {
   const clientTestimonials = [
     {
       id: 1,
       clientName: "Mr. Mahmoud Dauk",
-      clientDesignation: "IT Director, Al Mada ", 
+      clientDesignation: "IT Director, Al Mada ",
       thumbnail: "1",
       description:
         "GITL helped us in accelerating digital transformation journey with Business Central on Azure cloud and Power BI solution. As a result, our material requisition efficiency has increased by 66%, time taken in processing purchase orders is reduced by 79% and invoice processing efficiency has improved by 60%.",
-      category: "static", 
+      category: "static",
       link: "/",
     },
     {
       id: 2,
       clientName: "Mr. Shibu Mathew",
-      clientDesignation: "Principal Manager, JGC Gulf", 
+      clientDesignation: "Principal Manager, JGC Gulf",
       thumbnail: "2",
       description:
         "GITL upgraded Dynamics AX to Business Central on SAAS - and integrated Power BI seamlessly. The upgradation to an all-inclusive solution significantly helped us in taking control over our finance and budgeting processes.",
-      category: "video", 
+      category: "video",
       link: "/video",
     },
     {
       id: 3,
       clientName: "Rahul Mahajan",
-      clientDesignation: "Head of Operations, TCS", 
+      clientDesignation: "Head of Operations, TCS",
       thumbnail: "3",
       description:
         "Salesforce Sales Cloud implementation connected out complete lead-to-order cycle in customers' journey. GITL team proved their Infor LN expertise  and project management abilities by achieving highly challenging deadline.",
-      category: "static", 
+      category: "static",
       link: "/",
     },
     {
       id: 4,
       clientName: "Mr. Mahmoud Dauk",
-      clientDesignation: "IT Director, Al Mada ", 
+      clientDesignation: "IT Director, Al Mada ",
       thumbnail: "1",
       description:
         "GITL helped us in accelerating digital transformation journey with Business Central on Azure cloud and Power BI solution. As a result, our material requisition efficiency has increased by 66%, time taken in processing purchase orders is reduced by 79% and invoice processing efficiency has improved by 60%.",
-      category: "static", 
+      category: "static",
       link: "/",
     },
     {
       id: 5,
       clientName: "Mr. Shibu Mathew",
-      clientDesignation: "Principal Manager, JGC Gulf", 
+      clientDesignation: "Principal Manager, JGC Gulf",
       thumbnail: "2",
       description:
         "GITL upgraded Dynamics AX to Business Central on SAAS - and integrated Power BI seamlessly. The upgradation to an all-inclusive solution significantly helped us in taking control over our finance and budgeting processes.",
-      category: "video", 
+      category: "video",
       link: "/video",
     },
     {
       id: 6,
       clientName: "Rahul Mahajan",
-      clientDesignation: "Head of Operations, TCS", 
+      clientDesignation: "Head of Operations, TCS",
       thumbnail: "3",
       description:
         "Salesforce Sales Cloud implementation connected out complete lead-to-order cycle in customers' journey. GITL team proved their Infor LN expertise  and project management abilities by achieving highly challenging deadline.",
-      category: "static", 
+      category: "static",
       link: "/",
     }
-     
+
   ];
- 
-  
+
+
   return (
-    
+
     <>
       <div className="w-screen-lg mx-auto pl-[2rem] lg:px-0 lg:max-w-screen bg-[#F5F5F5]">
         <div className="pt-[5em] pb-[2em]">
@@ -108,7 +110,7 @@ const Testimonials = () => {
             {/* Slider Section */}
             <div className="col-span-1 col-start-1 col-end-2 lg:col-start-4 lg:col-span-4 ">
               <Swiper
-                className={`mySwiper pb-8`} 
+                className={`mySwiper pb-8`}
                 slidesPerView={1}
                 spaceBetween={10}
                 autoplay={{
@@ -116,7 +118,7 @@ const Testimonials = () => {
                   disableOnInteraction: false,
                 }}
                 pagination={{
-                  clickable: true, 
+                  clickable: true,
                 }}
                 breakpoints={{
                   640: {
@@ -125,36 +127,45 @@ const Testimonials = () => {
                   },
                   768: {
                     slidesPerView: 1,
-                    spaceBetween: 40,
+                    spaceBetween: 30,
                   },
                   1024: {
                     slidesPerView: 3,
-                    spaceBetween: 20,
+                    spaceBetween: 30,
                   },
                 }}
-                modules={[Autoplay, Pagination]} 
+                modules={[Autoplay, Pagination]}
               >
                 {
-                  clientTestimonials.map((ct,index)=>(
+                  clientTestimonials.map((ct, index) => (
                     <SwiperSlide className='min-w-[300px] pb-8'>
-                  <div className=''>
-                     <div>
-                        <Image src={`/testimonial-1.png`}
-                alt="blog post"
-                width="315"
-                height="200"/>
-                     </div>
-                     <div className=''>
-                     <p
-                className={` text-clip text-[#475467] text-sm pt-[16px]  pb-[14px]`}
-              >
-                {`“${ct.description}”`} 
-              </p>
-                     </div>
-                  </div>
-                </SwiperSlide>
+                      <div className=''>
+                        <div>
+                          <Image src={`/testimonial-${ct.id}.png`}
+                            alt="blog post"
+                            width="315"
+                            height="200" />
+                        </div>
+                        <div className=''>
+                          <p
+                            className={` text-clip text-[#475467] leading-[22px] text-sm pt-[16px]  pb-[14px]`}
+                          >
+                            {`“${ct.description}”`}
+                          </p>
+                        </div>
+                        <div className='flex flex-row justify-start items-start'>
+                          <div className='mr-4'>
+                            <Icon path={mdiDomain} size={1} />
+                          </div>
+                          <div>
+                            <p className='text-base font-medium text-[#101828] leading-[22px]'>{ct.clientName}</p>
+                            <p className='text-sm font-normal text-[#101828] leading-[22px]'>{ct.clientDesignation}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </SwiperSlide>
                   ))
-                } 
+                }
               </Swiper>
             </div>
           </div>
