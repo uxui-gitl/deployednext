2784a2caf152233b3c121fb76127ee9ddb377429
Krishna Sahu <dvlprkrishna@gmail.com>
2023-10-20 14:23:23 +0530

    fixed relative path issue

--- src/components/CardSwiper.jsx
+++ src/components/CardSwiper.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-<<<<<<< HEAD
 import { Scrollbar, Pagination } from "swiper/modules";
 import solution1 from "../../public/EntSolution/1.png";
 import solution2 from "../../public/EntSolution/2.png";
 import solution3 from "../../public/EntSolution/3.png";
-=======
-import { Scrollbar } from "swiper/modules";
-import solution1 from "../../public/EntSolution/1.png";
->>>>>>> fa5d34ca00b2347c65d71074c91f8c40f0846fc9
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
 
-<<<<<<< HEAD
 const styles = {
   cardWrap: {
     backgroundColor: "#07001F",
@@ -123,65 +117,6 @@ const CardSwiper = () => {
           </div>
         </SwiperSlide>
       </Swiper>
-=======
-const CardSwiper = () => {
-  return (
-    <>
-      <div className="">
-        <Swiper
-          data-cursor-color="#61dbfb"
-          data-cursor-text="Drag"
-          data-cursor-size="100px"
-          scrollbar={{
-            hide: true,
-          }}
-          modules={[Scrollbar]}
-          autoHeight={false}
-          centeredSlides={true}
-          spaceBetween={60}
-          grabCursor={true}
-          pagination={{
-            clickable: true,
-          }}
-        >
-          <SwiperSlide
-            className="p-5"
-            style={{
-              background:
-                "linear-gradient(270deg, rgba(91,98,138,1) 0%, rgba(120,111,192,1) 67%, rgba(148,112,197,1) 100%)",
-            }}
-          >
-            <div>
-              <Image src={solution1} alt="solution" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide
-            className="p-5"
-            style={{
-              background:
-                "linear-gradient(270deg, rgba(91,98,138,1) 0%, rgba(120,111,192,1) 67%, rgba(148,112,197,1) 100%)",
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-            necessitatibus amet magni nesciunt ducimus cumque pariatur illum
-            exercitationem assumenda eius obcaecati perspiciatis fugit
-            consequuntur culpa perferendis veritatis, incidunt id quisquam.
-          </SwiperSlide>
-          <SwiperSlide
-            className="p-5"
-            style={{
-              background:
-                "linear-gradient(270deg, rgba(91,98,138,1) 0%, rgba(120,111,192,1) 67%, rgba(148,112,197,1) 100%)",
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-            necessitatibus amet magni nesciunt ducimus cumque pariatur illum
-            exercitationem assumenda eius obcaecati perspiciatis fugit
-            consequuntur culpa perferendis veritatis, incidunt id quisquam.
-          </SwiperSlide>
-        </Swiper>
-      </div>
->>>>>>> fa5d34ca00b2347c65d71074c91f8c40f0846fc9
     </>
   );
 };
