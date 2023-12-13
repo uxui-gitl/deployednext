144bf3a9fa08f7c8184ccb7b4862e63a78bd12a3
tabassumSayyed <tabassum@godrej.com>
2023-12-13 11:37:54 +0530

    Revert Change

--- src/sections/intro/Intro.jsx
+++ src/sections/intro/Intro.jsx
@@ -45,7 +45,7 @@ const Intro = () => {
             </h1>
 
             <h1 className="sm:hidden sm:text-[68px] text-[34px] leading-[44px] sm:leading-[84px]	text-white font-bold text-center">
-              Delivering Business <br /> Value wit <br />{" "}
+              Delivering Business <br /> Value with <br />{" "}
               <TypeAnimation
                 className="typewriter-text text-white"
                 style={{ fontSize: "1em!important" }}
