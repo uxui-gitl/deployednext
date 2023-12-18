9f084d62d545aa2d96c3c238d43c721eaecfd62c
Krishna Sahu <dvlprkrishna@gmail.com>
2023-12-18 17:46:21 +0530

    Blog test

--- src/components/BlogSlider.jsx
+++ src/components/BlogSlider.jsx
@@ -11,6 +11,10 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 const BlogSlider = ({ arr }) => {
+  if (!arr || !Array.isArray(arr)) {
+    return null; // or handle this case as needed
+  }
+
   return (
     <>
       <div className={` w-full bg-white py-32`}>
@@ -22,49 +26,47 @@ const BlogSlider = ({ arr }) => {
             modules={[Pagination]}
           >
             {arr.map((item) => (
-              <>
-                <SwiperSlide key={item._id}>
-                  <div className="bg-white flex items-center flex-col-reverse sm:flex-row justify-start mb-10">
-                    <div className="text-left w-1/2">
-                      <h4
-                        className={` font-medium text-[#0745D3] uppercase ribbon`}
-                      >
-                        {item.ribbon}
-                      </h4>
-                      <h3 className="text-4xl leading-[42px] font-bold mb-5">
-                        {item.title}
-                      </h3>
-                      <p className="font-medium leading-[22px] md:w-[80%] ">
-                        {item.desc}
-                      </p>
-                      <Link
-                        href={item.link}
-                        className="flex text-[14px] text-[#0745D3] leading-[22px] mt-10 max-w-max border-b-[1px] border-[#FFFFFF] items-center font-medium hover:scale-105"
-                      >
-                        {item.cta}
-                        <span>
-                          <Icon
-                            path={mdiArrowRight}
-                            style={{
-                              marginLeft: "0.5em",
-                              width: "1rem",
-                              fontSize: "14px",
-                            }}
-                            className="cta-know-more"
-                          />
-                        </span>
-                      </Link>
-                    </div>
-                    <div>
-                      <Image
-                        src={blogpost}
-                        className="sm:mb-0 mb-8"
-                        alt="blog post"
-                      />
-                    </div>
+              <SwiperSlide key={item._id}>
+                <div className="bg-white flex items-center flex-col-reverse sm:flex-row justify-start mb-10">
+                  <div className="text-left w-1/2">
+                    <h4
+                      className={` font-medium text-[#0745D3] uppercase ribbon`}
+                    >
+                      {item.ribbon}
+                    </h4>
+                    <h3 className="text-4xl leading-[42px] font-bold mb-5">
+                      {item.title}
+                    </h3>
+                    <p className="font-medium leading-[22px] md:w-[80%] ">
+                      {item.desc}
+                    </p>
+                    <Link
+                      href={item.link}
+                      className="flex text-[14px] text-[#0745D3] leading-[22px] mt-10 max-w-max border-b-[1px] border-[#FFFFFF] items-center font-medium hover:scale-105"
+                    >
+                      {item.cta}
+                      <span>
+                        <Icon
+                          path={mdiArrowRight}
+                          style={{
+                            marginLeft: "0.5em",
+                            width: "1rem",
+                            fontSize: "14px",
+                          }}
+                          className="cta-know-more"
+                        />
+                      </span>
+                    </Link>
                   </div>
-                </SwiperSlide>
-              </>
+                  <div>
+                    <Image
+                      src={blogpost}
+                      className="sm:mb-0 mb-8"
+                      alt="blog post"
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
             ))}
           </Swiper>
         </div>
