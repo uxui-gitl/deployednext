515b6d655f8e9cfc10a80904d3d5c59c0b39aca2
Krishna Sahu <dvlprkrishna@gmail.com>
2023-12-18 16:40:48 +0530

    Changes about us

--- src/sections/aboutIntro/AboutIntro.jsx
+++ src/sections/aboutIntro/AboutIntro.jsx
@@ -20,16 +20,13 @@ const AboutIntro = () => {
       <div className="text-left max-w-screen-xl md:max-w-screen-2xl mb-5 mx-auto p-5 px-[2rem] grid grid-cols-2 gap-16">
         <div className="flex flex-col justify-center items-start ">
           <h3 className="leading-[52px] text-[42px] mb-3 font-bold">
-            BUILDING ROBUST TOMORROW FOR GLOBAL BUSINESSES WITH THE LEGACY OF INNOVATION
+            Building Robust Tomorrow For Global Businesses With The Legacy Of
+            Innovation
           </h3>
-          {/*<p className="font-medium leading-[22px] text-base md:w-[90%] mt-4 mb-10 ">
-            Revolutionize your business operations, save time and boost
-            productivity with our cutting-edge offerings.
-  </p>*/}
           <div>
             <a
               href="/"
-              className="text-[#101828] bg-[#F5F5F5] font-medium sm:font-normal rounded-sm	 capitalize max-w-max px-[2rem] sm:px-5 py-[0.55rem] sm:py-2 border flex"
+              className="text-[#101828] bg-[#F5F5F5] font-medium sm:font-normal rounded-sm   max-w-max px-[2rem] sm:px-5 py-[0.55rem] sm:py-2 border flex"
             >
               let&#39;s Connect
               <span>
