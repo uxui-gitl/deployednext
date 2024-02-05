a42772a8750cb1c406cf23a5502c00410c7ffb96
tabassumSayyed <tabassum@godrej.com>
2024-02-05 15:37:09 +0530

    5FebChanges

--- src/app/Solutions/(automation)/Intelligent-Technologies/AI-ML/page.jsx
+++ src/app/Solutions/(automation)/Intelligent-Technologies/AI-ML/page.jsx
@@ -280,7 +280,7 @@ const page = () => {
       {/* Our Ai Offerings */}
       <>
         <div className={` w-full bg-[#FFF] py-20`}>
-          <div className="text-left max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] grid grid-cols-1 gap-5 sm:grid-cols-2">
+          <div className="text-left max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] grid grid-cols-1 gap-5 sm:grid-cols-1">
             {/* left */}
             <div className="bg-[#FFF] flex items-start flex-col justify-between  relative">
               <div className="">
@@ -348,7 +348,7 @@ const page = () => {
       <>
         <div className={` w-full bg-[#F2F4F7] py-32`}>
           <div className="text-left max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] ">
-            <div className="bg-[#F2F4F7] grid grid-cols-1 gap-5 sm:grid-cols-2 relative">
+            <div className="bg-[#F2F4F7] grid grid-cols-1 gap-5 sm:grid-cols-1 relative">
               <div className="">
                 <h3 className="text-[42px]  leading-[54px] mb-3 font-bold ">
                   ML Expertise
