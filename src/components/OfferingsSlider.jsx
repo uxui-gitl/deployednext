708460874a03be55040efaaf934ccfe8b0e98d6e
Krishna Sahu <dvlprkrishna@gmail.com>
2024-02-14 15:06:03 +0530

    Retail industries

--- src/components/OfferingsSlider.jsx
+++ src/components/OfferingsSlider.jsx
@@ -1,7 +1,7 @@
 import Icon from "@mdi/react";
 import Link from "next/link";
 import React from "react";
-import { mdiArrowRight } from "@mdi/js";
+import { mdiArrowRight, mdiChevronUp } from "@mdi/js";
 import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
@@ -10,28 +10,81 @@ import "swiper/css/pagination";
 import Image from "next/image";
 import Accordion from "./Accordion";
 
-const OfferingsSlider = ({ arr, title, alignment, expandedDesc }) => {
+const OfferingsSlider = ({
+  arr,
+  title,
+  alignment,
+  expandedDesc,
+  alignHorizontal,
+  imageDisabled,
+  cardOnly,
+  desc,
+}) => {
   return (
     <>
       <div className={` w-full bg-[#FFF] py-20`}>
-        <div
-          className={`text-${alignment} max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] grid grid-cols-1 gap-5 sm:grid-cols-1`}
-        >
-          {/* left */}
-          <div className="bg-[#FFF] flex items-center flex-col justify-between  relative">
-            <div className="">
-              {title ? (
-                <h3 className="text-[42px] leading-[54px] mb-3 font-bold ">
-                  {title}
-                </h3>
-              ) : (
-                <h3 className="text-[42px] leading-[54px] mb-3 font-bold ">
-                  Our Offerings
-                </h3>
-              )}
+        {alignHorizontal ? (
+          <div
+            className={`text-${alignment} max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] grid grid-cols-1 gap-5 sm:grid-cols-1`}
+          >
+            {/* left */}
+            <div className="bg-[#FFF] flex items-start flex-col justify-between  relative">
+              <div className="">
+                {title ? (
+                  <h3 className="text-[42px] leading-[54px] mb-3 font-bold ">
+                    {title}
+                  </h3>
+                ) : (
+                  <h3 className="text-[42px] leading-[54px] mb-3 font-bold ">
+                    Our Offerings
+                  </h3>
+                )}
+              </div>
+            </div>
+
+            {/* right */}
+            <div className="bg-[#FFF] flex items-center flex-col justify-between  relative">
+              <div className="">
+                {desc ? (
+                  <p className="text-[16px] leading-[22px] mb-3 font-regular ">
+                    {desc}
+                  </p>
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div
+            className={`text-${alignment} max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] grid grid-cols-1 gap-5 sm:grid-cols-1`}
+          >
+            {/* left */}
+            <div className="bg-[#FFF] flex items-center flex-col justify-between  relative">
+              <div className="">
+                {title ? (
+                  <h3 className="text-[42px] leading-[54px] mb-3 font-bold ">
+                    {title}
+                  </h3>
+                ) : (
+                  <h3 className="text-[42px] leading-[54px] mb-3 font-bold ">
+                    Our Offerings
+                  </h3>
+                )}
+              </div>
+            </div>
+
+            {/* right */}
+            <div className="bg-[#FFF] flex items-center flex-col justify-between  relative">
+              <div className="">
+                {desc ? (
+                  <p className="text-[42px] leading-[54px] mb-3 font-bold ">
+                    {desc}
+                  </p>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        )}
+
         <div className="text-left max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] flex gap-10  overflow-hidden">
           <Swiper
             slidesPerView={"auto"}
@@ -63,71 +116,119 @@ const OfferingsSlider = ({ arr, title, alignment, expandedDesc }) => {
                 key={item._id}
                 className="w-fit md:w-1/3 max-w-[500px] mr-4  relative group"
               >
-                <div className="bg-[#EDF1FF] h-full  ">
-                  <div className=" h-full flex flex-col justify-start">
-                    <div className="w-full mb-4 ">
-                      <Image
-                        className="w-full"
-                        src={`/${item.image}`}
-                        height={200}
-                        width={290}
-                        alt=""
-                      />
-                    </div>
-                    <div className="flex justify-start flex-col h-full">
-                      <div className="p-4">
-                        <p className="text-[#101828] font-bold text-[30px] leading-[36px]">
-                          {item.title}
-                        </p>
-                        <p className="text-[#101828] text-[12px] font-normal my-4 leading-[18px]">
-                          {item.desc}
-                        </p>
-                      </div>
-                      <div>
-                        <div className="p-4 flex flex-col justify-end h-100">
-                          {item.subArr ? (
-                            <Accordion
-                              key={item._id}
-                              bullet={true}
-                              inline={item.inline}
-                              icons={false}
-                              items={item.subArrLinks}
-                              expandedDesc={expandedDesc}
-                              // items={[
-                              //   { _id: item._id, list: item.subArrLinks },
-                              // ]}
-                            />
-                          ) : (
-                            <ul className="my-4">
-                              {item.links &&
-                                item.links.map((list, index) => (
-                                  <li
-                                    key={index}
-                                    className="text-[#101828] text-[14px] font-medium border-b-2 border-[#DBDBDB] py-1 leading-[22px] w-fit  flex transition-all hover:opacity-75  "
-                                  >
-                                    {list.title}
-                                  </li>
-                                ))}
-                            </ul>
-                          )}
+                {/* If card display only */}
+                {cardOnly ? (
+                  <div className="bg-[#EDF1FF] h-full py-24">
+                    <div className=" h-full flex flex-col justify-start">
+                      {item.icon !== "" ? (
+                        <Image
+                          className="m-5"
+                          src={`/${
+                            (item.icon && item.icon + ".svg") || "dummy.png"
+                          }`}
+                          width={56}
+                          height={56}
+                          alt={item.icon}
+                        />
+                      ) : (
+                        <Icon path={mdiDomain} size={3} />
+                      )}
+                      <div className="flex justify-start flex-col h-full">
+                        <div className="p-4">
+                          <p className="text-[#101828] font-bold text-[30px] leading-[36px]">
+                            {item.title}
+                          </p>
+                          <p className="text-[#475467] text-[16px] font-normal my-4 leading-[18px]">
+                            {item.desc}
+                          </p>
                         </div>
-                        <div className="bg-[#0745D3] hidden  ">
-                          <Link
-                            href={item.ctaUrl}
-                            className="text-[#fff] w-full p-4 text-[14px] font-medium leading-[22px]   flex transition-all hover:opacity-75  "
-                          >
-                            {item.cta}{" "}
-                            <Icon
-                              path={mdiArrowRight}
-                              style={{ marginLeft: "0.5em" }}
-                              size={1}
-                            />
-                          </Link>
+                        <div>
+                          <div className="   ">
+                            <Link
+                              href={item.ctaUrl}
+                              className="text-[#0745D3] w-full p-4 text-[14px] font-medium leading-[22px]   flex transition-all hover:opacity-75  "
+                            >
+                              {item.cta}{" "}
+                              <Icon
+                                path={mdiArrowRight}
+                                style={{ marginLeft: "0.5em" }}
+                                size={1}
+                              />
+                            </Link>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
+                ) : (
+                  <div className="bg-[#EDF1FF] h-full  ">
+                    <div className=" h-full flex flex-col justify-start">
+                      {!imageDisabled ? (
+                        <div className="w-full mb-4 ">
+                          <Image
+                            className="w-full"
+                            src={`/${item.image}`}
+                            height={200}
+                            width={290}
+                            alt=""
+                          />
+                        </div>
+                      ) : null}
+                      <div className="flex justify-start flex-col h-full">
+                        <div className="p-4">
+                          <p className="text-[#101828] font-bold text-[30px] leading-[36px]">
+                            {item.title}
+                          </p>
+                          <p className="text-[#101828] text-[12px] font-normal my-4 leading-[18px]">
+                            {item.desc}
+                          </p>
+                        </div>
+                        <div>
+                          <div className="p-4 flex flex-col justify-end h-100">
+                            {item.subArr ? (
+                              <Accordion
+                                key={item._id}
+                                bullet={true}
+                                inline={item.inline}
+                                icons={false}
+                                items={item.subArrLinks}
+                                expandedDesc={expandedDesc}
+                                // items={[
+                                //   { _id: item._id, list: item.subArrLinks },
+                                // ]}
+                              />
+                            ) : (
+                              <ul className="my-4">
+                                {item.links &&
+                                  item.links.map((list, index) => (
+                                    <li
+                                      key={index}
+                                      className="text-[#101828] text-[14px] font-medium border-b-2 border-[#DBDBDB] py-1 leading-[22px] w-fit  flex transition-all hover:opacity-75  "
+                                    >
+                                      {list.title}
+                                    </li>
+                                  ))}
+                              </ul>
+                            )}
+                          </div>
+                          <div className="bg-[#0745D3] hidden  ">
+                            <Link
+                              href={item.ctaUrl}
+                              className="text-[#fff] w-full p-4 text-[14px] font-medium leading-[22px]   flex transition-all hover:opacity-75  "
+                            >
+                              {item.cta}{" "}
+                              <Icon
+                                path={mdiArrowRight}
+                                style={{ marginLeft: "0.5em" }}
+                                size={1}
+                              />
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </SwiperSlide>
             ))}
           </Swiper>
