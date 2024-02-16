d468143ba94ac4917afe102f1f1ab171d38fc734
tabassumSayyed <tabassum@godrej.com>
2024-02-16 18:19:57 +0530

    Infor WMS ver2 Changes

--- src/app/Solutions/(transformation)/Enterprise-Suite/Infor/Infor-WMS/page.jsx
+++ src/app/Solutions/(transformation)/Enterprise-Suite/Infor/Infor-WMS/page.jsx
@@ -277,13 +277,14 @@ const page = () => {
       </>
 
       {/* Infor WMS Key Highlights */}
+      {/* Infor WMS Key Highlights */}
       <>
-        <div className={`w-full bg-[#FFF] pt-32`}>
+        <div className={` w-full bg-[#FFF] pt-32`}>
           <div className="text-left max-w-screen-xl md:max-w-screen-xl mb-5 mx-auto p-5 pb-0 px-[2rem] grid grid-cols-1 gap-5 sm:grid-cols-2">
             {/* left */}
             <div className="bg-[#FFF] flex items-start flex-col justify-between  relative">
               <div className="">
-                <h3 className="text-[42px] leading-[54px] mb-3 font-bold w-[90%]">
+                <h3 className="text-[42px]  leading-[54px] mb-3 font-bold w-[90%]">
                   Infor WMS Key Highlights
                 </h3>
               </div>
@@ -292,7 +293,8 @@ const page = () => {
             <div>
               <div>
                 <p className="font-medium leading-[22px] md:w-[90%] ">
-                  Infor WMS is designed to optimize warehouse operations, simplifying complex processes with movement of goods tracking from receipt to dispatch. 
+                  Extending the capabilities of Infor with tailored functional
+                  enhancements for enhancement of business productivity
                 </p>
               </div>
             </div>
@@ -300,23 +302,52 @@ const page = () => {
         </div>
         <div className={`w-full pt-48 pb-12 bg-[url('/bgEcomScreen.png')]`}>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-10 justify-center">
-            {[0, 1, 2, 3].map((item, index) => {
+            {[
+              {
+                _id: 0,
+                title: "3D Visualization",
+                desc: "Experience streamlined operations with Infor WMS's 3D visual warehouse, providing real-time insights into bottlenecks and asset utilization.",
+                cta: "",
+                link: "",
+              },
+              {
+                _id: 1,
+                title: "Advanced Warehousing Processes",
+                desc: "Enhance efficiency with RF receiving, voice-enabled pick/pack operations, and dynamic put-away options.",
+                cta: "",
+                link: "",
+              },
+              {
+                _id: 2,
+                title: "3PL Billing",
+                desc: "Infor WMS integrates advanced activity-based features, enabling precise customer-level costing, billing, and invoicing. It supports multi-warehouse and multi-owner operations with tailored workflows and full traceability.",
+                cta: "",
+                link: "",
+              },
+              {
+                _id: 3,
+                title: "Labor Management & Configurability",
+                desc: "The user experience is tailored for productivity, enabling you to measure, assess, and optimize DC activities, identify bottlenecks, and maintain resource balance with engineered labor standards and performance metrics.",
+                cta: "",
+                link: "",
+              },
+            ].map((item, index) => {
               return (
                 <div key={index} className="mx-5 bg-white ">
                   <div className="p-5 w-fit ">
                     <h4 className="text-[26px] font-bold mb-2 leading-[34px] text-[#101828]">
-                      3D Visualization
+                      {item.title}
                     </h4>
                     <p className="text-[14px] leading-[22px] text-[#475467]">
-                     Experience streamlined operations with Infor WMS&#39;s 3D visual warehouse, providing real-time insights into bottlenecks and asset utilization. 
+                      {item.desc}
                     </p>
                   </div>
-                  <div className=" w-full bg-[#0745D3] p-5">
+                  <div className="hidden w-full bg-[#0745D3] p-5">
                     <Link
-                      href={"/"}
+                      href={item.link}
                       className="text-white  flex transition-all hover:opacity-75  "
                     >
-                      Know More{" "}
+                      {item.cta}
                       <Icon
                         path={mdiArrowRight}
                         style={{ marginLeft: "0.5em" }}
