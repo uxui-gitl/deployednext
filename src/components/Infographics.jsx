7bec90df6a34179cd5d9684d72ba9a8a5f260ce3
Krishna Sahu <dvlprkrishna@gmail.com>
2024-03-06 16:47:04 +0530

    Added Infographic zoom

--- src/components/Infographics.jsx
+++ src/components/Infographics.jsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import React from "react";
 import Infographic from "../../public/infographics/Infographic-Enterprise.png";
-
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 const Infographics = ({ src }) => {
   return (
     <>
       <div>
-        <Image
-          src={src}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-          alt="Infographic"
-        />
+        <Zoom>
+          <Image
+            src={src}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+            alt="Infographic"
+          />
+        </Zoom>
       </div>
     </>
   );
