804cb5e4e738f13f9c814e5727983fe51aa08a01
Krishna Sahu <dvlprkrishna@gmail.com>
2024-03-07 12:22:46 +0530

    SEO Meta Data

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,33 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import NextTopLoader from "nextjs-toploader";
 const works = Work_Sans({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+import Head from "next/head";
+
+const defaultMetadata = {
+  title:
+    "Godrej Infotech | Empowering Business Through Digital Transformation | Microsoft Dynamics, Infor, LS Central, Oracle",
+  description:
+    "Godrej Infotech, A G&B Group Company is a global leader in technology services and digital transformation with clients across Manufacturing, Retail, Transport and logistics, Oil & Gas Industies. We have been helping our clients across India, Middle East, Asia Pacific - Singapore, Philipines, Europe and USA with Innovative Solutions in the evolving world of ERP, Cloud, Digital & Platforms across technologies.",
+};
+
+function generateMetadata(route) {
+  const metadata = {
+    "/": {
+      title: "Home | Godrej Infotech",
+      description:
+        "Godrej Infotech, A G&B Group Company is a global leader in technology services and digital transformation with clients across Manufacturing, Retail, Transport and logistics, Oil & Gas Industies. We have been helping our clients across India, Middle East, Asia Pacific - Singapore, Philipines, Europe and USA with Innovative Solutions in the evolving world of ERP, Cloud, Digital & Platforms across technologies.",
+    },
+    "/Solutions/Intelligent-Technologies/AI-ML": {
+      title: "AI ML - Intelligent Technologies | Godrej Infotech",
+      description: "Learn more about our company",
+    },
+    // Add metadata for other routes
+  };
+
+  return metadata[route] || defaultMetadata;
+}
+
+export default function RootLayout({ children, route }) {
   const pathname = usePathname();
   const isHome = pathname === "/";
 
@@ -22,8 +48,15 @@ export default function RootLayout({ children }) {
     if (isLoading) return;
   }, [isLoading]);
 
+  const metadata = generateMetadata(pathname);
+
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* Add other meta tags as needed */}
+      </head>
       <body className={`${works.className} antialiased`}>
         {isLoading && isHome ? (
           <SplashScreen finishLoading={() => setIsLoading(false)} />
