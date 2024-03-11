2e5b01434b1720298901106a65d418b2864abfc4
Krishna Sahu <dvlprkrishna@gmail.com>
2024-03-11 10:49:20 +0530

    Middleware Test #1

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 
 const Middleware = (req) => {
-  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
-    return NextResponse.next();
+  if (req.nextUrl.pathname !== req.nextUrl.pathname.toLowerCase()) {
+    return NextResponse.redirect(
+      new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
+    );
+  }
 
-  return NextResponse.redirect(
-    new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
-  );
+  return NextResponse.next();
 };
 
 export default Middleware;
