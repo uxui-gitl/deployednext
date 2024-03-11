2d435d17e90697fd40926869bad84cc5bd62f1ee
Krishna Sahu <dvlprkrishna@gmail.com>
2024-03-11 11:02:52 +0530

    Middleware Testing #2

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,16 @@
 import { NextResponse } from "next/server";
 
 const Middleware = (req) => {
-  if (req.nextUrl.pathname !== req.nextUrl.pathname.toLowerCase()) {
-    return NextResponse.redirect(
-      new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
-    );
+  const isImageAsset = req.nextUrl.pathname.startsWith("/_next/image");
+  const lowercasedPath = req.nextUrl.pathname.toLowerCase();
+
+  if (req.nextUrl.pathname === lowercasedPath && !isImageAsset) {
+    return NextResponse.next(); // No redirect for pages and non-image assets
+  } else if (lowercasedPath !== req.nextUrl.pathname && !isImageAsset) {
+    return NextResponse.redirect(new URL(req.nextUrl.origin + lowercasedPath)); // Redirect only once for pages
   }
 
-  return NextResponse.next();
+  return NextResponse.next(); // No change for image assets
 };
 
 export default Middleware;
